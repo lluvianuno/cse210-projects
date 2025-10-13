3305d63a736e296510a9f8ae7f0768a390315dfe
lluvianuno <lluviajnuno@gmail.com>
2025-10-13 12:42:04 -0600

    W05 Learning Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment assignment1 = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
+        Console.WriteLine($"{assignment1.GetSummary()}");
+        Console.WriteLine($"{assignment1.GetHomeworkList()}");
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine($"{assignment2.GetWritingInformation()}");
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,14 @@
+class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_studentName} - {_topic}\n{_title}";
+    }
+}
